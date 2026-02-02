f22ef731b5190c7f7fe2576c2c3a1177c2669787
fredson <nkotanyifredson@gmail.com>
2026-02-02 22:31:37 +0200

    Initial commit

---
+++ demo/src/main/java/com/example/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if (password.length() < 8) {
+            out.println("Hello " + username + ", your password is weak. Try a strong one.");
+        } else {
+            out.println("Welcome " + username);
+        }
+    }
+}
